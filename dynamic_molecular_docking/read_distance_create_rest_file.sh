39b13251c2915f940e6c2e285699933b4bcf287b
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-03-27 16:48:03 +0100

    Consolidate read_distance_create_rest_file.sh

--- dynamic_molecular_docking/read_distance_create_rest_file.sh
+++ dynamic_molecular_docking/read_distance_create_rest_file.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+# Copyright 2013 Jan-Philip Gehrcke
 
+# Exit script upon first error.
+set -e
+
+AMBER_SETUP="/project/bioinfp_apps/amber12_centos58_intel1213_openmpi16_cuda5/setup.sh"
 CORE_ATOM_ID=$(cat core_atom_id)
 LIGAND_CENTER_ATOM_ID=$(cat ligand_center_atom_id)
 CORE_CENTER_TARGET_DISTANCE=$(cat core_center_target_distance)
-
-AMBER_SETUP="/apps11/bioinfp/amber12_centos58_intel1213_openmpi16_cuda5/setup.sh"
-
 DMDPRODRESTFILENAME="dmd_production.rest"
+DISTANCE_DATA_FILENAME="ligandcenter_protcore_distance.dat"
+EQUITRAJ_FILENAME="equilibrate_NPT.mdcrd"
+CPPTRAJ_INPUT_FILENAME="get_ligandcenter_protcore_distance.ptrajin"
+DISTANCE_VALUE_FILENAME="ligandcenter_protcore_distance"
 
-echo "Create DMD tMD restraints file ${DMDPRODRESTFILENAME}."
+echo "Plan: create DMD tMD restraints file ${DMDPRODRESTFILENAME}."
 echo " >> Sourcing ${AMBER_SETUP}"
 source ${AMBER_SETUP}
 
-DISTANCE_DATA_FILENAME="ligandcenter_protcore_distance.dat"
-
 CPPTRAJ_INPUT="
-trajin equilibrate_NPT.mdcrd lastframe
+trajin ${EQUITRAJ_FILENAME} lastframe
 distance datasetname @${CORE_ATOM_ID} @${LIGAND_CENTER_ATOM_ID} out ${DISTANCE_DATA_FILENAME} geom noimage
 go
 "
 
-CPPTRAJ_INPUT_FILENAME="get_ligandcenter_protcore_distance.ptrajin"
 echo "${CPPTRAJ_INPUT}" > ${CPPTRAJ_INPUT_FILENAME}
-
-
 echo " >> Wrote ${CPPTRAJ_INPUT_FILENAME}: "
 echo "$(cat ${CPPTRAJ_INPUT_FILENAME})"
 echo
@@ -41,7 +42,6 @@ echo " >> Wrote ${DISTANCE_DATA_FILENAME}: "
 echo "$(cat ${DISTANCE_DATA_FILENAME})"
 echo
 
-DISTANCE_VALUE_FILENAME="ligandcenter_protcore_distance"
 cat ${DISTANCE_DATA_FILENAME} | tail -n 1 | gawk '{print $2}' > ${DISTANCE_VALUE_FILENAME}
 
 echo " >> Remove ${DISTANCE_DATA_FILENAME}"
@@ -52,7 +52,6 @@ echo "$(cat ${DISTANCE_VALUE_FILENAME})"
 echo
 
 INITIAL_DISTANCE=$(cat ${DISTANCE_VALUE_FILENAME})
-
 DMDPRODRESTFILE_CONTENT="
 &rst
     iat=${CORE_ATOM_ID}, ${LIGAND_CENTER_ATOM_ID},
@@ -61,10 +60,7 @@ DMDPRODRESTFILE_CONTENT="
     rk2=100,
 &end
 "
-
 echo "${DMDPRODRESTFILE_CONTENT}" > ${DMDPRODRESTFILENAME}
-
 echo "wrote ${DMDPRODRESTFILENAME}: "
 echo "$(cat ${DMDPRODRESTFILENAME})"
-echo 
-
+echo
