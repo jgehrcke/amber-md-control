4762686e7f470f60318c1459ede4e12f6150439e
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-04-29 18:58:40 +0200

    03_aggregate_mmpbsa_data.sh: adjust legacy script to new system.

--- dynamic_molecular_docking/analysis/03_aggregate_mmpbsa_data.sh
+++ dynamic_molecular_docking/analysis/03_aggregate_mmpbsa_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright (C) 2012 Jan-Philip Gehrcke
+#   Copyright (C) 2012-2013 Jan-Philip Gehrcke
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -25,7 +25,28 @@ log() {
     echo "INFO  >>> $@"
     }
 
-PREFIX="/home/bioinfp/jang/project/md/smd/docking/1bfb/md/atlas_state_120903"
+PREFIX="../06_md"
+
+
+# Extract mmpbsa_200ps_after_freemd score
+OUTFILE="mmpbsa_freemd_last100frames_all_ligands.dat"
+log "Creating $OUTFILE ..."
+# Overwrite file
+echo "run_id,mmpbsa_freemdlast100frames_deltag,mmpbsa_freemdlast100frames_deltag_stddev,mmpbsa_freemdlast100frames_deltaeel,mmpbsa_freemdlast100frames_deltaeel_stddev" > $OUTFILE
+find ${PREFIX} -wholename "*tmd_*/freemd/mmpbsa_last100frames/FINAL_RESULTS_MMPBSA.dat" | \
+while read FILE
+do
+    log "Processing file '$FILE' ..."
+    RUNID=$(echo "$FILE" | utils/collect_pdb_files_with_run_id.py --print-run-ids)
+    DELTAG=$(cat $FILE | grep "DELTA TOTAL" | awk '{print $3}')
+    DELTAG_STDDEV=$(cat $FILE | grep "DELTA TOTAL" | awk '{print $4}')
+    EEL=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $2}')
+    EEL_STDDEV=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $3}')
+    # Append to file.
+    echo "${RUNID},${DELTAG},${DELTAG_STDDEV},${EEL},${EEL_STDDEV}" >> $OUTFILE
+done
+
+exit
 
 # Extract mmpbsa_after_smd score
 OUTFILE="mmpbsa_after_smd_all_ligands.dat"
@@ -40,24 +61,6 @@ do
     EEL=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $2}')
     # Append to file.
     echo "${RUNID},${DELTAG},${EEL}" >> $OUTFILE
-done
-
-
-# Extract mmpbsa_200ps_after_freemd score
-OUTFILE="mmpbsa_200ps_after_freemd_all_ligands.dat"
-log "Creating $OUTFILE ..."
-# Overwrite file
-echo "run_id,mmpbsa_200ps_after_freemd_deltag,mmpbsa_200ps_after_freemd_deltag_stddev,mmpbsa_200ps_after_freemd_deltaeel,mmpbsa_200ps_after_freemd_deltaeel_stddev" > $OUTFILE
-find ${PREFIX} -wholename "*SMD_PROD_*/free_md/mmpbsa_last100fr_with2ps_interval/FINAL_RESULTS_MMPBSA.dat" | \
-while read FILE
-do
-    RUNID=$(../scripts/run_id_from_path.py $FILE)
-    DELTAG=$(cat $FILE | grep "DELTA G binding" | awk '{print $5}')
-    DELTAG_STDDEV=$(cat $FILE | grep "DELTA G binding" | awk '{print $7}')
-    EEL=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $2}')
-    EEL_STDDEV=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $3}')
-    # Append to file.
-    echo "${RUNID},${DELTAG},${DELTAG_STDDEV},${EEL},${EEL_STDDEV}" >> $OUTFILE
 done
 
 
