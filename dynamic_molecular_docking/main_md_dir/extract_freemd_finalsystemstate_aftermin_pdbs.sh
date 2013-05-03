9a623288bbef5f9c1175b04c2bf2aec89618980e
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-05-03 18:38:19 +0200

    /extract_freemd_finalsystemstate_aftermin_pdbs.sh: if final minimization restart file exists, extract PDB. No other criteria.

--- dynamic_molecular_docking/main_md_dir/extract_freemd_finalsystemstate_aftermin_pdbs.sh
+++ dynamic_molecular_docking/main_md_dir/extract_freemd_finalsystemstate_aftermin_pdbs.sh
@@ -19,6 +19,11 @@ STARTDIR="$PWD"
 SCRIPT_TO_EXECUTE="./trajframe_to_pdb_using_stripmask.sh"
 ABSPATH_TO_SCRIPT=$(readlink -f ${SCRIPT_TO_EXECUTE})
 
+if [ ! -f "$ABSPATH_TO_SCRIPT" ]; then
+    echo "No such file: '${ABSPATH_TO_SCRIPT}'" 1>&2
+    exit 1
+fi
+
 # Set up environment (Amber, Python, ...).
 if [ -f "./env_setup.sh" ]; then
     source "./env_setup.sh"
@@ -26,18 +31,21 @@ fi
 
 echo "Execute script for each finished freeMD: ${ABSPATH_TO_SCRIPT}"
 
-./find_unfinished_freemd_trajectories.sh --finished-only | while read FREEMDDIR
+# Filter started free MDs (not finished), i.e. if after_freemd_min2.rst exists
+# in free MD dir, then extract PDB. It's up to the final minimization script
+# to decide which free MD states are 'final' enough to be minimized. (At the time of
+# writing this comment, after_freemd_min2.rst has been created from free MD
+# trajectories with at least 2200 frames (while 2500 was the goal).
+RST_FILE="after_freemd_min2.rst"
+./find_unfinished_freemd_trajectories.sh --started-only | while read FREEMDDIR
 do
     cd "$FREEMDDIR"
-    echo "Working in $FREEMDDIR (swallowing stdout)..."
-
-    # ERROR >>> 1st arg: Amber topology file.
-    # ERROR >>> 2nd arg: NetCDF trajectory file.
-    # ERROR >>> 3rd arg: cpptraj frame selection, e.g. '2 2' or 'lastframe'.
-    # ERROR >>> 4th arg: ambmask selecting the atoms to strip before writing PDB file.
-    # ERROR >>> 5th arg: output PDB filename.
-
-    ${ABSPATH_TO_SCRIPT} top.prmtop after_freemd_min2.rst lastframe ':WAT,Cl-,Na+' freemd_final_system_state_aftermin.pdb 1> /dev/null < /dev/null
+    echo "Working in $FREEMDDIR ..."
+    if [ ! -f "$RST_FILE" ];then
+        echo "No $RST_FILE, skipping."
+        continue
+    fi
+    ${ABSPATH_TO_SCRIPT} top.prmtop "$RST_FILE" lastframe ':WAT,Cl-,Na+' freemd_final_system_state_aftermin.pdb 1> /dev/null < /dev/null
     if [ $? -ne 0 ]; then
         echo "Error observed. Abort freeMD dir iteration."
         exit 1
