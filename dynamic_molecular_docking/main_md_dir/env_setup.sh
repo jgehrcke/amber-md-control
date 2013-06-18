0a5a3563edcb173ec3e2669290516d3b8e92b854
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-06-18 13:17:15 +0200

    Add error checks to env_setup.sh.

--- dynamic_molecular_docking/main_md_dir/env_setup.sh
+++ dynamic_molecular_docking/main_md_dir/env_setup.sh
@@ -6,13 +6,15 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "environment setup from directory ${THIS_SCRIPT_DIR}."
 export DMD_CODE_DIR="${THIS_SCRIPT_DIR}/../amber-md-control/dynamic_molecular_docking"
 
-# Template for the environment setup file. Adjust accordingly for
-# the HPC environment.
+if [ ! -d "$DMD_CODE_DIR" ]; then
+    echo "DMD_CODE_DIR is not a directory: $DMD_CODE_DIR" 1>&2;
+    exit 1
+fi
 
 AMBER_SETUP="/projects/bioinfp_apps/amber12_at13_centos58_intel1213_ompi164_cuda5/setup.sh"
 PYTHON_SETUP="/projects/bioinfp_apps/Python-2.7.3/setup.sh"
 echo "Sourcing $AMBER_SETUP"
-source "${AMBER_SETUP}"
+source "${AMBER_SETUP}" || exit 1
 echo "Sourcing $PYTHON_SETUP"
-source "${PYTHON_SETUP}"
+source "${PYTHON_SETUP}" || exit 1
 
