f2d323cad2fc74480be2594fd49f95bbe0c52f51
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-06-19 18:13:28 +0200

    Implement dataset merging in pandas based on concat rather than join.

--- dynamic_molecular_docking/analysis/merge_per_run_data.py
+++ dynamic_molecular_docking/analysis/merge_per_run_data.py
@@ -3,15 +3,9 @@
 # Copyright 2012-2013 Jan-Philip Gehrcke, BIOTEC, TU Dresden
 # http://gehrcke.de
 
-import os
-import sys
-import logging
 import glob
-
+import logging
 import pandas as pd
-import numpy as np
-
-PER_RUN_DATA_DIR = "per_run_data"
 
 logging.basicConfig(
     format='%(asctime)s:%(msecs)05.1f  %(levelname)s: %(message)s',
@@ -19,20 +13,20 @@ logging.basicConfig(
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
+
 def main():
-    datafile_paths = glob.glob("%s/*.dat" % PER_RUN_DATA_DIR)
-    for i, datafile_path in enumerate(datafile_paths):
-        log.info("Processeing files '%s'." % datafile_path)
-        if not i:
-            merged_dataframe = pd.read_csv(datafile_path, index_col="run_id")
-            continue
-        merged_dataframe = merged_dataframe.join(
-            pd.read_csv(datafile_path, index_col="run_id"))
+    datafile_paths = glob.glob("per_run_data/*.dat")
+    for datafile_path in datafile_paths:
+        log.info("Found file '%s'." % datafile_path)
+    dataframes = [pd.read_csv(p, index_col="run_id") for p in datafile_paths]
+    merged_dataframe = pd.concat(dataframes, axis=1)
+
+    # Alternative method, for reference: Use DataFrame's join multiple times:
+    # merged_dataframe = merged_dataframe.join(
+    #        pd.read_csv(datafile_path, index_col="run_id"))
 
     outfile_path = "per_run_data_merged.dat"
-    if os.path.exists(outfile_path):
-        log.info("Output file '%s' will be overwritten." % outfile_path)
-
+    log.info("Writing output file '%s'." % outfile_path)
     merged_dataframe.to_csv(outfile_path)
 
 
