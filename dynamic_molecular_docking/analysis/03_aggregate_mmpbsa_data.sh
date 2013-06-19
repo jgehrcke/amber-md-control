47bcc797bbfc163bdb99c4b38ec3e5343efa4561
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-06-19 15:00:15 +0200

    Implement improved/fixed version of MMPBSA analysis script.

--- dynamic_molecular_docking/analysis/03_aggregate_mmpbsa_data.sh
+++ dynamic_molecular_docking/analysis/03_aggregate_mmpbsa_data.sh
@@ -14,17 +14,18 @@ fi
 source "${DMD_CODE_DIR}/common_code.sh"
 set -e
 
-
-# Extract mmpbsa_200ps_after_freemd score
-OUTFILE="mmpbsa_freemd_last100frames_all_ligands.dat"
+LAST_N=250
+OUTFILE="mmpbsa_freemd_last${LAST_N}frames.dat"
 log "Creating $OUTFILE ..."
 # Overwrite file
-echo "run_id,mmpbsa_freemdlast100frames_deltag,mmpbsa_freemdlast100frames_deltag_stddev,mmpbsa_freemdlast100frames_deltaeel,mmpbsa_freemdlast100frames_deltaeel_stddev" > $OUTFILE
-find ${PREFIX} -wholename "*tmd_*/freemd/mmpbsa_last100frames/FINAL_RESULTS_MMPBSA.dat" | \
+echo "run_id,mmpbsa_freemdlast${LAST_N}frames_deltag,mmpbsa_freemdlast${LAST_N}frames_deltag_stddev,mmpbsa_freemdlast${LAST_N}frames_deltaeel,mmpbsa_freemdlast${LAST_N}frames_deltaeel_stddev" > $OUTFILE
+find ${MD_DIR} -wholename "*tmd_*/freemd/mmpbsa_last${LAST_N}frames/FINAL_RESULTS_MMPBSA.dat" | \
 while read FILE
 do
     log "Processing file '$FILE' ..."
     RUNID=$(echo "$FILE" | utils/collect_pdb_files_with_run_id.py --print-run-ids)
+    # The code below has been validated to work with the output of MMPBSA.py
+    # of AmberTools 13, patched version June 19, 2013
     DELTAG=$(cat $FILE | grep "DELTA TOTAL" | awk '{print $3}')
     DELTAG_STDDEV=$(cat $FILE | grep "DELTA TOTAL" | awk '{print $4}')
     EEL=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $2}')
@@ -34,25 +35,3 @@ do
 done
 
 exit
-
-# Extract mmpbsa_after_smd score
-OUTFILE="mmpbsa_after_smd_all_ligands.dat"
-log "Creating $OUTFILE ..."
-# Overwrite file
-echo "run_id,mmpbsa_after_smd_deltag,mmpbsa_after_smd_deltaeel" > $OUTFILE
-find ${PREFIX} -regextype posix-extended -regex ".*SMD_PROD_.*[[:digit:]]\/mmpbsa_lastframe/FINAL_RESULTS_MMPBSA.dat" | \
-while read FILE
-do
-    RUNID=$(../scripts/run_id_from_path.py $FILE)
-    DELTAG=$(cat $FILE | grep "DELTA G binding" | awk '{print $5}')
-    EEL=$(cat $FILE | tail -n20 | head -n6 | grep EEL | awk '{print $2}')
-    # Append to file.
-    echo "${RUNID},${DELTAG},${EEL}" >> $OUTFILE
-done
-
-
-
-
-
-
-
