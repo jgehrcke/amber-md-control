060e00d015048c57e9ad7d9e8f73d6e890712e2a
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-06-19 13:15:38 +0200

    Implement calculation of distance between receptor and ligand: average over minimal distances for each ligand atom.

--- dynamic_molecular_docking/analysis/05_measure_lig_rec_distances_after_freemd.sh
+++ dynamic_molecular_docking/analysis/05_measure_lig_rec_distances_after_freemd.sh
@@ -20,14 +20,15 @@ RECEPTOR_RESIDUES=$(cat "${MD_DIR}/receptor_residues")
 STARTDIR=$(pwd)
 
 # Define filename for data file to be created in analysis directory.
-OUTFILE="lig_rec_mindistance_after_freemd.dat"
+OUTFILE="lig_rec_distances_after_freemd.dat"
 
 # Define filename for data file to be created in every single free MD dir,
 # containing only the numerical value of the minimal distance in Angstrom.
-DISTFILE="receptor_ligand_min_distance_after_freemd_min"
+MINDISTFILE="receptor_ligand_min_distance_after_freemd_min"
+AVGDISTFILE="receptor_ligand_avg_distance_after_freemd_min"
 log "Creating $OUTFILE ..."
 # Overwrite file
-echo "run_id,lig_rec_mindistance_after_freemd_min" > $OUTFILE
+echo "run_id,lig_rec_min_distance_after_freemd_min,lig_rec_avg_distance_after_freemd_min" > $OUTFILE
 find ${MD_DIR} -wholename "*tmd_*/freemd/freemd_final_system_state_aftermin.pdb" | \
 while read FILE
 do
@@ -58,24 +59,40 @@ ligand_residues = [system_chain[x] for x in ligand_res_numbers]
 receptor_residues = [system_chain[x] for x in receptor_res_numbers]
 
 # Create collections of atom objects, one for receptor, one for ligand.
-r_atom_coords = [a.get_coord() for r in receptor_residues for a in r]
-l_atom_coords = [a.get_coord() for l in ligand_residues for a in l]
+r_atoms_coords = [a.get_coord() for r in receptor_residues for a in r]
+l_atoms_coords = [a.get_coord() for l in ligand_residues for a in l]
 
 # Calculate all pairwise distances between both sets of atoms.
-distancematrix = distance.cdist(r_atom_coords, l_atom_coords, 'euclidean')
+distancematrix = distance.cdist(r_atoms_coords, l_atoms_coords, 'euclidean')
 
-# Retrieve minimal distance, write it to output file.
+# Retrieve distance between ligand and receptor: use minimal distance between
+# any ligand atom and any receptor atom.
 min_distance = np.min(distancematrix)
-with open('$DISTFILE', 'w') as f:
+with open('$MINDISTFILE', 'w') as f:
     f.write(str(min_distance))
+
+# Retrieve distance between ligand and receptor: averaged over all ligand
+# atoms, whereas for each ligand atom the minimal distance to the receptor
+# is determined.
+# In the distance matrix, each column contains distances for one
+# ligand atom to all receptor atoms. np.min(A, axis=0) determines the
+# minum value for each column, resulting in a one-dimensional array.
+# Ref: SciPy docs: scipy/reference/generated/scipy.spatial.distance.cdist.html
+#      "For each i and j, the metric dist(u=XA[i], v=XB[j])
+#       is computed and stored in the ij th entry"
+avg_distance = np.mean(np.min(distancematrix, axis=0))
+with open('$AVGDISTFILE', 'w') as f:
+    f.write(str(avg_distance))
 EOF
     if [ $? -ne 0 ]; then
         err "Distance extraction failed unexpectedly. Exit."
         exit 1
     fi
-    MINDISTANCE=$(cat "$DISTFILE")
+    MINDISTANCE=$(cat "$MINDISTFILE")
+    AVGDISTANCE=$(cat "$AVGDISTFILE")
     log "minimal distance: $MINDISTANCE Angstroms."
+    log "average distance: $AVGDISTANCE Angstroms."
     cd "$STARTDIR"
     # Append data to output file.
-    echo "${RUNID},${MINDISTANCE}" >> $OUTFILE
+    echo "${RUNID},${MINDISTANCE},${AVGDISTANCE}" >> $OUTFILE
 done
