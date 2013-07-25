18ba89de74ff50b36226657ab0768cdfed1d7bc1
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-07-25 14:33:15 +0200

    mmpbsa_freemd_lastNframes_all_freemd_dirs.sh: new cmdline parsing and use new work dir identification script.

--- dynamic_molecular_docking/main_md_dir/mmpbsa_freemd_lastNframes_all_freemd_dirs.sh
+++ dynamic_molecular_docking/main_md_dir/mmpbsa_freemd_lastNframes_all_freemd_dirs.sh
@@ -5,8 +5,27 @@
 STARTDIR="$PWD"
 SCRIPT_TO_EXECUTE="./mmpbsa_freemd_lastNframes.sh"
 ABSPATH_TO_SCRIPT=$(readlink -f ${SCRIPT_TO_EXECUTE})
-NBR_CPUS="$1"
-BATCH_SYSTEM="$2"
+
+BATCH_SYSTEM=""
+DRYRUN=false
+
+for i in "$@"
+do
+case $i in
+    --nbr-cpus=*)
+    NBR_CPUS=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --batch-system=*)
+    BATCH_SYSTEM=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --dry)
+    DRYRUN=true
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
 
 # Set up environment (Amber, Python, ...).
 if [ -f "./env_setup.sh" ]; then
@@ -14,25 +33,31 @@ if [ -f "./env_setup.sh" ]; then
 fi
 
 if [ -z "$NBR_CPUS" ]; then
-    echo "First argument: number of CPUs (required)."
+    echo "--nbr-cpus=N argument required."
     exit 1
 fi
 
 echo "execute script in each free MD dir: ${ABSPATH_TO_SCRIPT}"
 
-./find_unfinished_freemd_trajectories.sh --finished-only | while read FREEMDDIR
+./find_freemd_dirs_without_mmpbsa_result.sh | while read FREEMDDIR
 do
     cd "$FREEMDDIR"
     echo "Working in $FREEMDDIR (swallowing stdout)..."
     # Some MMPBSA-related process reads from STDIN and therefore swallows
     # the output of find_unfinished... provided to `while read...` above.
     # Give MMPBSA some STDIN to read from in order to keep the loop intact.
-    if [[ "$BATCH_SYSTEM" == "--sge" ]]; then
+
+    if ${DRYRUN}; then
+        echo "Would run here."
+        continue
+    fi
+
+    if [[ "$BATCH_SYSTEM" == "sge" ]]; then
         qsub -pe smp $NBR_CPUS -cwd -V -q bioinfp.q -b yes -o mmpbsa_sge.log -j y "/bin/bash ${ABSPATH_TO_SCRIPT} $NBR_CPUS" < /dev/null
-    elif [[ "$BATCH_SYSTEM" == "--lsf" ]]; then
+    elif [[ "$BATCH_SYSTEM" == "lsf" ]]; then
         echo "Not implemented for LSF. Exit."
         exit 1
-    elif [[ "$BATCH_SYSTEM" == "--slurm" ]]; then
+    elif [[ "$BATCH_SYSTEM" == "slurm" ]]; then
         echo "Not implemented for Slurm. Exit."
         exit 1
     else
