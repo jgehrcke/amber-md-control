245269dc5e6c13f99240404c763456d3f3eafb6a
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-08-12 18:12:46 +0200

    Add all-dir-dihedral-analyzer.

---
+++ dynamic_molecular_docking/main_md_dir/analyze_freemd_cpptraj_dihedrals_all_freemd_dirs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2012-2013 Jan-Philip Gehrcke, BIOTEC, TU Dresden
+# http://gehrcke.de
+
+SCRIPT_TO_EXECUTE="./analyze_freemd_cpptraj_dihedrals.sh"
+
+
+# Set up environment (Amber, Python, ...).
+if [ -f "./env_setup.sh" ]; then
+    source "./env_setup.sh"
+fi
+STARTDIR="$PWD"
+ABSPATH_TO_SCRIPT=$(readlink -f ${SCRIPT_TO_EXECUTE})
+echo "Execute script in each free MD dir: ${ABSPATH_TO_SCRIPT}"
+
+#./find_unfinished_freemd_trajectories.sh --minframes 2200 | while read FREEMDDIR
+./find_unfinished_freemd_trajectories.sh --finished-only | while read FREEMDDIR
+do
+cd "$FREEMDDIR"
+echo "Working in '$FREEMDDIR' (swallowing stdout)..."
+${ABSPATH_TO_SCRIPT} 1> /dev/null < /dev/null
+if [ $? -ne 0 ]; then
+    echo "Error observed. Abort free MD dir iteration."
+    exit 1
+fi
+cd "$STARTDIR"
+done
