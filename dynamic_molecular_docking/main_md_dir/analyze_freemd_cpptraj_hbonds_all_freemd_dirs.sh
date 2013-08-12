1988efd3630a9dab495f6ed8674d11e737d477c8
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-08-12 17:34:27 +0200

    Work on analyze_freemd_cpptraj*all*.dat files.

--- dynamic_molecular_docking/main_md_dir/analyze_freemd_cpptraj_hbonds_all_freemd_dirs.sh
+++ dynamic_molecular_docking/main_md_dir/analyze_freemd_cpptraj_hbonds_all_freemd_dirs.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# Copyright 2012-2013 Jan-Philip Gehrcke, BIOTEC, TU Dresden
+# http://gehrcke.de
+
+SCRIPT_TO_EXECUTE="./analyze_freemd_cpptraj_hbonds.sh"
 
 # Set up environment (Amber, Python, ...).
 if [ -f "./env_setup.sh" ]; then
     source "./env_setup.sh"
 fi
-
 STARTDIR="$PWD"
-SCRIPT_TO_EXECUTE="./analyze_freemd_cpptraj_hbonds.sh"
 ABSPATH_TO_SCRIPT=$(readlink -f ${SCRIPT_TO_EXECUTE})
+echo "Execute script in each free MD dir: ${ABSPATH_TO_SCRIPT}"
 
-echo "execute script in each free MD dir: ${ABSPATH_TO_SCRIPT}"
-
-./find_unfinished_freemd_trajectories.sh --minframes 2200 | while read FREEMDDIR
-#./find_unfinished_freemd_trajectories.sh --finished-only | while read FREEMDDIR
+#./find_unfinished_freemd_trajectories.sh --minframes 2200 | while read FREEMDDIR
+./find_unfinished_freemd_trajectories.sh --finished-only | while read FREEMDDIR
 do
 cd "$FREEMDDIR"
-echo "Working in $FREEMDDIR (swallowing stdout)..."
+echo "Working in '$FREEMDDIR' (swallowing stdout)..."
 ${ABSPATH_TO_SCRIPT} 1> /dev/null < /dev/null
 if [ $? -ne 0 ]; then
     echo "Error observed. Abort free MD dir iteration."
@@ -23,4 +24,3 @@ if [ $? -ne 0 ]; then
 fi
 cd "$STARTDIR"
 done
-
