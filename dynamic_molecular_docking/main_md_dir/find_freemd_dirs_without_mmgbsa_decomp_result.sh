beb433db65d91bc99ee35806529da6bacc87b5e4
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-01-02 12:51:01 +0100

    Add --deletedir option to find_freemd_dirs_without_mmgbsa_decomp_result.sh.

--- dynamic_molecular_docking/main_md_dir/find_freemd_dirs_without_mmgbsa_decomp_result.sh
+++ dynamic_molecular_docking/main_md_dir/find_freemd_dirs_without_mmgbsa_decomp_result.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-#
-#   Copyright (C) 2012, 2013 Jan-Philip Gehrcke
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-
+# Copyright 2012-2014 Jan-Philip Gehrcke, BIOTEC, TU Dresden
+# http://gehrcke.de
 
 #set -e
 
 LAST_N=250
 SEARCH_IN_FREEMD_DIR="mmgbsa_decomp_last${LAST_N}frames/FINAL_DECOMP_MMPBSA.dat"
+DELETEDIRPATH="mmgbsa_decomp_last${LAST_N}frames"
+
 FREEMDOUTFILE="production_NVT.out"
 FREEMDDIR="freemd"
+
+REMOVEDIR=false
+for i in "$@"
+do
+case $i in
+    --deletedir)
+    REMOVEDIR=true
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
 
 for LIGDIR in ligand_*; do
     if [ -d "$LIGDIR" ]; then
@@ -53,6 +54,14 @@ for LIGDIR in ligand_*; do
             fi
             # free MD finished, but MMPBSA result file not in place.
             echo $PWD
+            if ${REMOVEDIR}; then
+                if [ -d "$DELETEDIRPATH" ]; then
+                    echo "Removing directory $DELETEDIRPATH"
+                    rm -rf "$DELETEDIRPATH"
+                else
+                    echo "Removing directory $DELETEDIRPATH: dir does not exist."
+                fi
+            fi
         fi
         cd ../../ ;
     done;
