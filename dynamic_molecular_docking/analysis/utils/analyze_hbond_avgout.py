9cbbb7764847d00ef48f7ac28b8903509241e533
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-06-09 17:44:30 +0200

    analysis: hbond_avgout.py: really use Agg MPL backend.

--- dynamic_molecular_docking/analysis/utils/analyze_hbond_avgout.py
+++ dynamic_molecular_docking/analysis/utils/analyze_hbond_avgout.py
@@ -12,10 +12,9 @@ import argparse
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
-
+import matplotlib.pyplot as plt
 
 logging.basicConfig(
     format='%(asctime)s:%(msecs)05.1f  %(levelname)s: %(message)s',
