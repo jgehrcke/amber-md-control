47a1d16676458f7093ebd4f0a7af764e6de8fdad
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-07-16 16:42:17 +0200

    SRED: fix axis limit.

--- dynamic_molecular_docking/analysis/utils/merge_mmgbsa_decomp_data.py
+++ dynamic_molecular_docking/analysis/utils/merge_mmgbsa_decomp_data.py
@@ -178,11 +178,11 @@ def plot_top_residues(
         #rotation=45
         )
     plt.xlim([-1, plot_N])
-    
-    if residue_names[0] in RESNAMEMAP:
+    if residue_names[0][1].isdigit():
         # Define y limit (mainly for thesis plots, remove afterwards...).
-        # Do only for receptor (via RESNAMEMAP check hack).
-        plt.ylim([0, -16])
+        # Do only for receptor (via isdigit hack, cause ligand resnames are 3 letters).
+        print "JA KLAR IST DAS DABEI"
+        plt.ylim([-16, 0])
     plt.xlabel('%s residue' % reclig)
     plt.ylabel(
         u'$\\langle \mathrm{\Delta G} \\rangle$ [kcal/mol]')
